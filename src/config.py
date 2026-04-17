5147df35447d60fbd40d3d5ac604484313af1afa
Kaarthick <kaarthickrajakumar@gmail.com>
2026-04-18 01:36:11 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 def get_backend():
     
-    token = "bWWllLM0ZCJh8mSUh-WVCWhOVrIUdHaiInTP1mKHys0e" 
+    token = "Your Token here" 
     service = QiskitRuntimeService(channel="ibm_quantum_platform", token=token)
     return service.least_busy(simulator=False)
